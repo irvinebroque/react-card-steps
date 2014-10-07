6dbce7b76db1e17a81677aad22e6828c608c37ba
Brendan Irvine-Broque <brendanib@gmail.com>
2014-10-06 17:35:54 -0700

    this is a step

--- index.js
+++ index.js
@@ -4,6 +4,15 @@
 
 var React = require('react');
 
+function getSteps(step) {
+  return (
+    <Step
+      key={step.id}
+      step={step}
+    />
+  );
+}
+
 var ReactCardSteps = React.createClass({
 
   displayName: 'ReactCardSteps',
@@ -28,15 +37,68 @@ var ReactCardSteps = React.createClass({
   },
 
   render: function() {
+
+    console.log(this.props.stepData);
+
+    var steps = this.props.stepData.map(getSteps);
+
     return (
-      <div>foo</div>
+      <div>
+        {steps}
+      </div>
     )
   }
 
 
 });
 
+var Step = React.createClass({
+
+  displayName: 'Step',
+
+  propTypes: {
+    step: React.PropTypes.object
+  },
+
+  getDefaultProps: function() {
+    return {
+      stepData: {}
+    }
+  },
+
+  render: function() {
+    return (
+      <div>
+        this is a step
+      </div>
+    );
+  }
+
+});
+
+var exampleSteps = [
+  {
+    id: 1,
+    text: "step 1"
+  },
+  {
+    id: 2,
+    text: "step 2"
+  },
+  {
+    id: 3,
+    text: "step 3"
+  },{
+    id: 4,
+    text: "step 4"
+  },
+  {
+    id: 5,
+    text: "step 5"
+  }
+]
+
 React.renderComponent(
-    <ReactCardSteps />,
+    <ReactCardSteps stepData={exampleSteps} />,
     document.getElementById('react')
 );
